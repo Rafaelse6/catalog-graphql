e137a473f8f33b7721d3902d8550f7751d548713
Rafael Santos <rafa.05125@gmail.com>
2023-04-28 11:47:09 -0300

    Project created

---
+++ graphqlcatalog/src/main/java/com/rafaelsantos/graphqlcatalog/GraphqlcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelsantos.graphqlcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlcatalogApplication.class, args);
+	}
+
+}
