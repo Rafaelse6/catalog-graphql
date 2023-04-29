d581b5721c76877ee6afd8018ab70efefbafe1b8
Rafael Santos <rafa.05125@gmail.com>
2023-04-29 15:40:51 -0300

    Category GraphQL schema

--- graphqlcatalog/src/main/java/com/rafaelsantos/graphqlcatalog/modules/category/CategoryController.java
+++ graphqlcatalog/src/main/java/com/rafaelsantos/graphqlcatalog/modules/category/CategoryController.java
@@ -1,7 +1,31 @@
 package com.rafaelsantos.graphqlcatalog.modules.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @MutationMapping
+    CategoryEntity addCategory(@Argument CategoryInput category){
+       var categoryCreated = this.categoryRepository.save(new CategoryEntity(category.name));
+       return categoryCreated;
+    }
+
+    @QueryMapping
+    Optional<CategoryEntity> categoryById(@Argument UUID id){
+        var category = this.categoryRepository.findById(id);
+        return category;
+    }
+
+    record CategoryInput(String name){}
 }
