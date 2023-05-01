031af3e077520e4725dc5be4a0f7022b7e50b919
Rafael Santos <rafa.05125@gmail.com>
2023-04-30 22:17:27 -0300

    Product entity, repository, controller

---
+++ graphqlcatalog/src/main/java/com/rafaelsantos/graphqlcatalog/modules/product/ProductEntity.java
@@ -0,0 +1,34 @@
+package com.rafaelsantos.graphqlcatalog.modules.product;
+
+import com.rafaelsantos.graphqlcatalog.modules.category.CategoryEntity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", insertable = false, updatable = false)
+    private CategoryEntity category;
+
+    @Column(name = "category_id")
+    private UUID categoryId;
+
+    public ProductEntity(String name, BigDecimal price, UUID categoryId){
+        this.name = name;
+        this.price = price;
+        this.categoryId = categoryId;
+    }
+}
